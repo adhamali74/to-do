d07b8e2c6c8782b77fb7b201605af9e4bc17bd6a
Adham Abdelfattah <50050146+adhamali74@users.noreply.github.com>
2024-02-19 00:40:28 +0100

    POST req added

--- models/tasks.js
+++ models/tasks.js
@@ -5,6 +5,6 @@ const mongoose = require("mongoose");
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: false },
-  completed: { type: Boolean, required: false },
+  completed: { type: Boolean, default: false },
 });
 module.exports = mongoose.model("Task", taskSchema);
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -13,4 +13,14 @@ router.get("/tasks", async (req, res) => {
   }
 });
 
+router.post("/tasks", async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(200).json({ message: "Added task successfully!!", task });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
 module.exports = router;
